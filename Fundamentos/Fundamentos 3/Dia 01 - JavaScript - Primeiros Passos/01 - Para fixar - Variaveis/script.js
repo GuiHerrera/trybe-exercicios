be7b38218899f78276edd24d74e01d4b7af1783a
Guilherme Herrera <gui-herrera@hotmail.com>
2023-03-30 23:16:21 -0300

    fundamentos3.1 e reorganização das pastas e arquivos

---
+++ Fundamentos/Fundamentos 3/Dia 01 - JavaScript - Primeiros Passos/01 - Para fixar - Variaveis/script.js
@@ -0,0 +1,5 @@
+let myName = "Guilherme";
+let birthCity = "Taubaté-SP";
+let birthYear = 38;
+
+console.log(myName, birthCity, birthYear);
---
+++ Fundamentos/Fundamentos 3/Dia 01 - JavaScript - Primeiros Passos/04 -Operadores Logicos/operadorOR.js
@@ -0,0 +1,11 @@
+let weekDay = "quarta-feira";
+if (
+  weekDay === "segunda-feira" ||
+  "terça-feira" ||
+  "quinta-feira" ||
+  "sexta-feira"
+) {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+  console.log("FINALMENTE, desanso merecido! UwU!");
+}

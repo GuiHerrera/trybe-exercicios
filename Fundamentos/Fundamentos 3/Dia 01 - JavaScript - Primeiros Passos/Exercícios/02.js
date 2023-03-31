0c35a57812c853fbffb4728b32ce032de5d6e53e
Guilherme Herrera <gui-herrera@hotmail.com>
2023-03-31 20:18:51 -0300

    Exercicios Dia 01: JavaScript - Primeiros passos.

---
+++ Fundamentos/Fundamentos 3/Dia 01 - JavaScript - Primeiros Passos/Exercícios/02.js
@@ -0,0 +1,10 @@
+// 2 - Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+
+let num1 = 7;
+let num2 = 9;
+
+if (num1 > num2) {
+  console.log(num1 + " é maior que " + num2);
+} else {
+  console.log(num2 + " é maior que " + num1);
+}

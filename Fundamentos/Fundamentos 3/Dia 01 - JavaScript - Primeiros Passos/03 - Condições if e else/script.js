c59eb2548c8b7d6b05b45d01785c91015dd8585e
Guilherme Herrera <gui-herrera@hotmail.com>
2023-03-31 00:29:12 -0300

    Inserido exercicio para fixar  03 - Condições if e else

---
+++ Fundamentos/Fundamentos 3/Dia 01 - JavaScript - Primeiros Passos/03 - Condições if e else/script.js
@@ -0,0 +1,9 @@
+let notaCandidata = 75;
+
+if (notaCandidata >= 80) {
+  console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+} else if (notaCandidata < 80 && notaCandidata >= 60) {
+  console.log("Você está na nossa lista de espera.");
+} else {
+  console.log("Infelizmente, você reprovou.");
+}

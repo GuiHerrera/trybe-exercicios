7e7d3a6844d4d30a42d441a223b9ccd3fbbb4f34
Guilherme Herrera <gui-herrera@hotmail.com>
2023-04-04 22:32:28 -0300

    Exercício Bônus 2

--- Fundamentos/Fundamentos 3/Dia 05 - JavaScript ES6 - let, const, arrow functions e template literals 🚀/03 - Exercícios - Bônus/01 - funcao1.js
+++ Fundamentos/Fundamentos 3/Dia 05 - JavaScript ES6 - let, const, arrow functions e template literals 🚀/03 - Exercícios - Bônus/01 - funcao1.js
@@ -20,7 +20,42 @@ const substituaX = (nome) => {
       fraseArray[index] = nome;
     }
   }
-  return fraseArray.join("");
+  return fraseArray.join(" ");
 };
 
-console.log(substituaX("Bebeto"));
+// console.log(substituaX("Bebeto"));
+
+// Função 2: Escreva uma função que receberá o retorno da Função 1 por parâmetro e retornará uma nova string.
+
+// O nome da função deverá ser minhasSkills.
+
+// A função deverá receber o retorno da Função 1 - substituaX por parâmetro.
+
+// Declare dentro da função uma variável com o nome skills e do tipo const.
+
+// A variável skills deverá ser um array que contenha três strings com tecnologias que você já estudou.
+// Crie uma varável do tipo let e concatene o valor retornado da Função 1 - substituaX (utilize template literals), a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+
+// Exemplo de retorno:
+
+// Tryber Bebeto aqui!
+
+// Minhas três principais habilidades são:
+
+// JavaScript
+// HTML
+// CSS
+
+const minhasSkills = (funcao1) => {
+  const skills = ["JavaScript", "HTML", "CSS"];
+  let frase = `${funcao1}
+  Minhas três principais habilidades são:
+  - ${skills[0]}
+  - ${skills[1]}
+  - ${skills[2]}`;
+
+  return frase;
+};
+
+const funcao1 = substituaX("Guilherme");
+console.log(minhasSkills(funcao1));
